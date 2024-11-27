7350b6ee19487ee1e23abf5fd366e86bd0a7d23e
JOSERSU <salinasuribejose@gmail.com>
2024-11-26 21:37:39 -0600

    first commit

---
+++ app/src/main/java/com/example/examen_ii/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.examen_ii
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val users = findViewById<Button>(R.id.btnUsers)
+        val creditos = findViewById<Button>(R.id.btnCreditos)
+        val salir = findViewById<Button>(R.id.btnSalir)
+
+
+        users.setOnClickListener {
+            startActivity(Intent(this, UsersActivity::class.java))
+        }
+
+        creditos.setOnClickListener{
+            startActivity(Intent(this, CreditosActivity::class.java))
+        }
+
+        salir.setOnClickListener {
+            finishAffinity()
+        }
+
+
+
+    }
+}
